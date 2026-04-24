02c90ef62085270ea7b1de7b5252f1b1f4cc9f75
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-04-24 08:46:28 +0500

    Adding the solution of furthest point from the origin

---
+++ c++/FurthestPoint.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int n=moves.size();
+        int left{},right{},dashed{};
+        for(int i{};i<n;++i)
+        {
+            if(moves[i]=='L')
+            left++;
+            else if(moves[i]=='R')
+             right++;
+             else
+             dashed++;
+        }
+        return abs(left-right)+dashed;
+    }
+};
