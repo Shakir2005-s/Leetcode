7efbdf1976bea458f6aff7c185be9f4c8613734c
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-05-01 21:39:56 +0500

    Adding the optimized solution for rotation function problem

---
+++ c++/rotate.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int length=nums.size();
+       int sum{}, F{};
+
+        for(int i{};i<length;++i)
+        {
+            sum+=nums[i];
+            F+=i*nums[i];
+        }
+           int maxi = F;
+
+        for (int i = 1; i < length; i++) {
+            F += sum - length * nums[length - i];
+            maxi = max(maxi, F);
+        }
+
+        return maxi;
+    }
+};
