c079adf3ac0d0195b678dc0f13f656b0b99b6863
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-05-02 21:37:05 +0500

    I added the brute force solution of rotate digit by 180 problem

---
+++ c++/rotateDigit.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int count{};
+        
+        for(int i{1};i<=n;++i)
+        {
+            bool isDiff=false;
+            bool isValid=true;
+            int num=i;
+            while(num>0)
+            {
+               int rem=num%10;
+               if(rem==2||rem==5||rem==6||rem==9)
+               {
+                isDiff=true;
+               }
+                else if(rem==3||rem==4||rem==7)
+                {
+                    isValid=false;
+                    break;
+                }
+                num=num/10;
+
+            }
+            if(isValid&&isDiff)
+            count++;
+        }
+        return count;
+        
+    }
+};
