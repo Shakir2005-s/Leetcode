f4f49d1552a61229e63f91758d2380119b5f9a47
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-05-04 22:29:21 +0500

    I added the solution of optimized solution of rotate image by 90 degree

---
+++ c++/RotateImage.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i{};i<n;++i)
+        {
+            for(int j{i};j<n;++j)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                else
+                {
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+            }
+        }
+        for(int i{};i<n;++i)
+        {
+            int k=0,l=n-1;
+            while(k<l)
+            {
+                swap(matrix[i][k],matrix[i][l]);
+                ++k;
+                --l;
+            }
+        }
+        
+    }
+};
