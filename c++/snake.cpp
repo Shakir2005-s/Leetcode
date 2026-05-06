106395ae72b00e21b847830ce3dd89a9fb2e32d6
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-05-06 21:49:54 +0500

    i added the snake problem on codeforces

--- c++/snake.cpp
+++ c++/snake.cpp
@@ -7,12 +7,24 @@ int main() {
     
     for (int i = 1; i <= n; i++) {
         if (i % 2 == 1) {
+            
             for (int j = 0; j < m; j++) {
                 cout << '#';
             }
         } else {
-            for (int j = 0; j < m; j++) {
-                cout << '.';
+           
+            if ((i / 2) % 2 == 1) {
+              
+                for (int j = 0; j < m - 1; j++) {
+                    cout << '.';
+                }
+                cout << '#';
+            } else {
+                
+                cout << '#';
+                for (int j = 1; j < m; j++) {
+                    cout << '.';
+                }
             }
         }
         cout << '\n';
