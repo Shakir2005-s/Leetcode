6279f8cd74c7e948fb0bea76742298689d0c7635
Shakir Hussain <shakirhussainkumicki@gmail.com>
2026-05-26 21:41:39 +0500

    add the solution of count special characters problem

---
+++ c++/specailCharacter.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int numberOfSpecialChars(string word)
+    {
+        int n = word.size();
+        unordered_map<char, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[word[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (word[i] >= 'a' && word[i] <= 'z' && m[word[i]] > 0)
+            {
+                if (m[word[i]] >= 1 && m[toupper(word[i])] >= 1)
+                {
+                    count++;
+                    m[word[i]] = 0;
+                }
+            }
+        }
+        return count;
+    }
+};
